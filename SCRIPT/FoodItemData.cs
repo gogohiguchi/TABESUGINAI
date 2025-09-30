f54ebbe26881f69a6a61f999134d2df0f65c598c
241039 <jojohiguti.ivy.github.io>
2025-09-30 13:35:04 +0900

    no message

--- SCRIPT/FoodItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Food Item", menuName = "Game Data/Food Item")]
-public class FoodItemData : ScriptableObject
-{
-    public string foodName; // 食材の名前
-    public float fullnessValue; // 満腹ゲージの増加量
-    public int scoreValue; // スコアの増加量
-}
